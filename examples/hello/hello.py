8ac07e09909b29b263391748c52755a2de851a84
tecnobillo <30511567+tecnobillo@users.noreply.github.com>
2022-01-28 12:35:48 +0100

    Add files via upload

---
+++ examples/hello/hello.py
@@ -0,0 +1,11 @@
+import os; os.chdir(os.path.split(__file__)[0])
+import sys; sys.path.append('../../')
+
+# The above is only necessary because termo is not in the python path
+
+import termo
+
+@termo.app(__file__, gui='hello-gui.py')
+class AppBackend:
+    def get_user(self):
+        return os.getlogin()
