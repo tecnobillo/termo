d31eac195a404ff44be862a429f5c39f0ed7efff
tecnobillo <30511567+tecnobillo@users.noreply.github.com>
2022-01-28 13:13:31 +0100

    Update hello-gui.py

--- examples/hello/hello-gui.py
+++ examples/hello/hello-gui.py
@@ -1,17 +1,7 @@
 import termo
-from browser import document, alert
-from browser.html import *
-
-import sys
-
-class Err:
-    def write(self, err):
-        alert(err)
-
-sys.stderr = Err()
+from browser import document
+from browser.html import H1
 
 termux_user = termo.app.get_user()
 
 document <= H1(f'Hello {termux_user}!')
-
-alert(f'Hello {termux_user}!')
