6f6d4e3407f0e2090b7e017f4e7d60fdf8ea18f6
tecnobillo <estarache@gmail.com>
2022-03-23 12:26:33 +0100

    show-toast modification

--- examples/show-toast/show-toast.py
+++ examples/show-toast/show-toast.py
@@ -4,7 +4,7 @@ import os; os.chdir(os.path.split(__file__)[0])
 import sys; sys.path.append('../../')
 
 # The above is only necessary because termo is not in the python path
-# To use termux-toast install Termux:API (android) and 'pkg install termux-api' (termux=
+# To use termux-toast install Termux:API (android) and 'pkg install termux-api' (termux)
 
 import termo
 
